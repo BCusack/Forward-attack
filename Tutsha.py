803153bffe8dc87617a03d666660476a099854ae
Brian Cusack <briancusack78@gmail.com>
2021-12-03 16:42:48 +1100

    Update Tutsha.py

--- Tutsha.py
+++ Tutsha.py
@@ -5,13 +5,11 @@ import hashlib
 import os
 os.system('cls' if os.name == 'nt' else 'clear')
 # Example of SHA256 forward attack using a list of known hashes indexed by position
-# Get Bob's Hash
-# Enter Bob's Hash
+# Get Alice's Hash
+# Enter Alice's Hash
 # get inputs to check
 
 # Create a list of known hashes in range
-
-
 def createStore(start, end):
     store_list = []
     for number in range(start, end):
@@ -20,31 +18,27 @@ def createStore(start, end):
     return store_list
 
 # Check the hash against the list of known hashes for positioning
-
-
 def CheckHashList(input_start, store_list, input_hash):
     if input_hash in store_list:
         for i, item in enumerate(store_list):
             if item == input_hash:
-                print("Bob's number is: " + str(input_start + i))
+                print("Alice's number is: " + str(input_start + i))
                 break
     else:
-        print("Bob's number is not in the list")
+        print("Alice's number is not in the list")
 
 # Input logic
-
-
 def run(createStore):
     print('If we know the starting value')
     input_start = int(input("Enter the starting number: "))
     print('And we know the ending value')
     input_end = int(input("Enter the ending number: "))
     print('We can then check the hashes against a list of known hashes')
-    input_number = int(input("Enter Bob's Number To Be Hashed: "))
+    input_number = int(input("Enter Alice's Number To Be Hashed: "))
     store_list = createStore(input_start, input_end)
-    print('Copy and paste the hash of the number or and hash you want to check')
+    print('Copy and paste the hash of the number or any hash you want to check')
     print(hashlib.sha256(str(input_number).encode('utf-8')).hexdigest())
-    input_hash = str(input("Enter Bob's Hash: ")).lower()
+    input_hash = str(input("Enter Alice's Hash: ")).lower()
     return input_start, store_list, input_hash
 
 
